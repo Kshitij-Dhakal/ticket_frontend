c4aa46dafec3ad66645530d41ce54b6de8fa0249
Kshitij Dhakal <dhakalkshitij@gmail.com>
2022-07-23 00:32:36 +0545

    route to dashboard added

--- src/app/dashboard/dashboard-router.module.ts
+++ src/app/dashboard/dashboard-router.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Route, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard.component';
 
-
+const routes: Route[] = [
+  { path: '', component: DashboardComponent },
+]
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
+  ],
+  exports: [
+    RouterModule
   ]
 })
 export class DashboardRouterModule { }
